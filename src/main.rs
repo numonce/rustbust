590bff914f136aecb8441c570254f64c41a0f127
numonce <numonce@protonmail.com>
2022-09-30 10:52:18 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use clap::{Arg, Command};
+use std::io::BufRead;
+use std::sync::Arc;
+use tokio::sync::Semaphore;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Command::new("RustBuster")
+        .author("numonce")
+        .version("1.0.0")
+        .about("A simple dirb clone in Async Rust!")
+        .arg(
+            Arg::new("wordlist")
+                .takes_value(true)
+                .required(true)
+                .long("wordlist")
+                .short('w')
+                .help("Path to wordlist to be used"),
+        )
+        .arg(
+            Arg::new("url")
+                .takes_value(true)
+                .required(true)
+                .long("url")
+                .short('u')
+                .help("Url to be used"),
+        )
+        .get_matches();
+
+    let cli = reqwest::Client::builder()
+        .timeout(std::time::Duration::from_millis(5000))
+        .build()?;
+    let urlbuff = app.value_of("url").unwrap().to_owned();
+    let wordfile = std::fs::File::open(app.value_of("wordlist").unwrap());
+    let reader = std::io::BufReader::new(wordfile.unwrap());
+    let semaphore = Arc::new(Semaphore::new(120));
+    let tasks: Vec<_> = reader
+        .lines()
+        .map(|word| {
+            let newcli = cli.clone();
+            let url = urlbuff.clone();
+            let permit = semaphore.clone();
+            tokio::spawn(async move {
+                let _holder = permit.acquire().await.unwrap();
+                request(url, word.unwrap(), newcli).await.unwrap();
+            })
+        })
+        .collect();
+
+    for task in tasks {
+        task.await?;
+    }
+    Ok(())
+}
+
+async fn request(
+    url: String,
+    word: String,
+    client: reqwest::Client,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("{}/{}", url, word);
+    let res = client.get(&url).send().await;
+    if let Ok(res) = res {
+        if res.status() != 404 {
+            println!("Testing {} and the status is {}", url, res.status());
+        }
+    }
+
+    Ok(())
+}
+    
